9b935b8fdd3577d7103dae550990d4db1320273d
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-06-14 23:38:35 +0900

    01-05

--- level_editor.py
+++ level_editor.py
@@ -1,8 +1,8 @@
 import bpy
-
+import math
 #ブレンダーに登録するアドオン情報
 bl_info={
-"name":"レベルエディタ",
+"neme":"レベルエディタ",
 "author":"satou yui",
 "version":(1,0),
 "blender":(3,3,1),
@@ -12,15 +12,145 @@ bl_info={
 "wiki_url":"",
 "tracker_url":"",
 "category":"Object"
-
 }
+
+
+#メニュー項目描画
+def draw_menu_manual(self,context):
+    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
+    #context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+    
+    #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+    self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
+    
+
+
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #blenderがクラスの識別するための固有の文字列
+    bl_idname="TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label="MyMenu"
+    #著者表示用の文字列
+    bl_description="拡張メニュー by"+bl_info["author"]
+    
+    #サブメニューの描画
+    def draw(self,context):
+        
+        #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
+        # トップバーの「エディターメニュー」に項目（オペレータ）を追加
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname,text=MYADDON_OT_create_ico_sphere.bl_label)
+        # トップバーの「エディターメニュー」に項目（オペレータ）を追加
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname,text=MYADDON_OT_export_scene.bl_label)
+    
+    
+    #既存のメニューにサブメニューを追加
+    def submenu(self,context):
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+    
+
+#オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+
+#
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname="myaddon.myaddon_otcreate_object"
+    bl_label="ICO球生成します"
+    bl_options={'REGISTER','UNDO'}
+    
+    #メニューを実行したときに呼ばれる関数
+    def execute(self,context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+        
+        return{'FINISHED'}
+
+#オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self, context):
+
+        print("シーン情報をExportします")
+
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+
+            print(object.type + " - " + object.name)
+
+            #ローカルトランスフォームを取得（行列から回転・スケーリングを抽出）
+            #Vector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+
+            #回転はクォータニオンからEuler（3軸での回転角）に変換
+            rot = rot.to_euler()
+
+            #ラジアンから度に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+       
+            #トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+
+            #親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+   
+            print()
+
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
+
+#blenderに登録するクラスリフト
+classes=(
+MYADDON_OT_stretch_vertex,
+MYADDON_OT_create_ico_sphere,
+MYADDON_OT_export_scene,
+TOPBAR_MT_my_menu,
+)
+
 
 #アドオン有効化時コールバック
 def register():
+  
+    #
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    #メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
 
 #アドオン無効化時コールバック
 def unregister():
+    #メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+   #
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました")
 
 #
