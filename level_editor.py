66e14bc3d33df3178f9671ecfa094d8f8280dfc5
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-06-14 23:32:24 +0900

    01_05

--- level_editor.py
+++ level_editor.py
@@ -53,21 +53,21 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     
 
 
-#オペレータ　頂点を伸ばす
+#オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
-    bl_idname="myaddon.myaddon_ot_stretch_vertex"
-    bl_label="頂点を伸ばす"
-    bl_description="頂点座標を引っ張って伸ばします"
-    #リドゥ、アンドゥ可能オブジェクト
-    bl_options={'REGISTER','UNDO'}
-    
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
     #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self,context):
-        bpy.data.objects["Cube"].data.vertices[0].co.x+=1.0
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
         print("頂点を伸ばしました。")
-        
+
         #オペレータの命令終了を通知
-        return{'FINISHED'}
+        return {'FINISHED'}
 
 #
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
