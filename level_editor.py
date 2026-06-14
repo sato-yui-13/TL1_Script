737129092b77820ffe21ed8f93fd4f4666e8f322
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-06-14 15:18:50 +0900

    01_05

--- level_editor.py
+++ level_editor.py
@@ -2,7 +2,7 @@ import bpy
 
 #ブレンダーに登録するアドオン情報
 bl_info={
-"name":"レベルエディタ",
+"neme":"レベルエディタ",
 "author":"satou yui",
 "version":(1,0),
 "blender":(3,3,1),
@@ -12,15 +12,60 @@ bl_info={
 "wiki_url":"",
 "tracker_url":"",
 "category":"Object"
-
 }
+
+
+#メニュー項目描画
+def draw_menu_manual(self,context):
+    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
+    #context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+    
+    #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+    self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
+    
+
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #blenderがクラスの識別するための固有の文字列
+    bl_idname="TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label="MyMenu"
+    #著者表示用の文字列
+    bl_description="拡張メニュー by"+bl_info["author"]
+    
+    #サブメニューの描画
+    def draw(self,context):
+        
+        #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+        self.layout.operator("wm.url_open_preset",text="Manual",icon='HELP')
+    
+    #既存のメニューにサブメニューを追加
+    def submenu(self,context):
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+    
+
+#blenderに登録するクラスリフト
+classes=(TOPBAR_MT_my_menu,)
 
 #アドオン有効化時コールバック
 def register():
+  
+    #
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    #メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
 
 #アドオン無効化時コールバック
 def unregister():
+    #メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+   #
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました")
 
 #
