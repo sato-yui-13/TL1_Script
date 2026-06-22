54cf02fcdd388952acac601291dc4402810cff21
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-06-22 12:37:59 +0900

    01_07

--- level_editor.py
+++ level_editor.py
@@ -1,8 +1,11 @@
 import bpy
 import math
+import bpy_extras
+
+
 #ブレンダーに登録するアドオン情報
 bl_info={
-"neme":"レベルエディタ",
+"name":"レベルエディタ",
 "author":"satou yui",
 "version":(1,0),
 "blender":(3,3,1),
@@ -52,7 +55,6 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
     
 
-
 #オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_stretch_vertex"
@@ -83,17 +85,86 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         return{'FINISHED'}
 
 #オペレータ シーン出力
-class MYADDON_OT_export_scene(bpy.types.Operator):
+class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelper):
     bl_idname = "myaddon.myaddon_ot_export_scene"
     bl_label = "シーン出力"
     bl_description = "シーン情報をExportします"
 
+    #出力するファイルの拡張子
+    filename_ext = ".scene"
+    
+
     def execute(self, context):
 
         print("シーン情報をExportします")
+        
+        #ファイルに出力
+        self.export()
+        
+        
+        self.report({'INFO'}, "シーン情報をExportしました")
+        print("シーン情報をExportしました")
+        
+        return {'FINISHED'}
+    
+        
+    def export(self):
+        """ファイルに出力"""
 
-        #シーン内の全オブジェクトについて
-        for object in bpy.context.scene.objects:
+        print("シーン情報出力開始... %r" % self.filepath)
+
+        #ファイルをテキスト形式で書き出し用にオープン
+        #スコープを抜けると自動的にクローズされる
+        with open(self.filepath, "wt") as file:
+
+            #ファイルに文字列を書き込む
+            file.write("SCENE")
+            #シーン内の全オブジェクトについて
+            for object in bpy.context.scene.objects:
+            
+                if(object.parent):
+                    continue
+                self.parse_scene_recursive(file, object, 0)
+             
+    def write_and_print(self, file, str):
+        print(str)
+        file.write(str)
+        file.write('\n')
+       
+              
+    def parse_scene_recursive(self, file, object, level):
+        """シーン解析用再帰関数"""
+        
+        indent=''
+        for i in range(level):
+            indent +="\t"
+
+        #オブジェクト名書き込み
+        self.write_and_print(file, indent+ object.type + " - " + object.name)
+
+        trans, rot, scale = object.matrix_local.decompose()
+
+        #回転をクォータニオンからEuler（3軸での回転角）に変換
+        rot = rot.to_euler()
+
+        #ラジアンから度に変換
+        rot.x = math.degrees(rot.x)
+        rot.y = math.degrees(rot.y)
+        rot.z = math.degrees(rot.z)
+
+        #トランスフォーム情報を表示
+        self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+        self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+        self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+
+        self.write_and_print(file, '')
+        
+        #子ノードへ進む(深さが1上がる)
+        for child in object.children:
+            self.parse_scene_recursive(file, child, level + 1)
+        
+        
+            
 
             print(object.type + " - " + object.name)
 
@@ -121,10 +192,47 @@ class MYADDON_OT_export_scene(bpy.types.Operator):
             print()
 
 
-        print("シーン情報をExportしました")
-        self.report({'INFO'}, "シーン情報をExportしました")
+#パネル ファイル名
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル """
+    bl_idname="OBJECT_PT_file_name"
+    bl_label="FileName"
+    bl_space_type="PROPERTIES"
+    bl_region_type="WINDOW" 
+    bl_context="object"
+    
+    #サブメニューの描画
+    def draw(self,context):
 
-        return {'FINISHED'}
+        #パネルに項目を追加
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,text=MYADDON_OT_stretch_vertex.bl_label)
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname,text=MYADDON_OT_create_ico_sphere.bl_label)
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname,text=MYADDON_OT_export_scene.bl_label)
+
+        if"file_name" in context.object:
+            #既にプロパティがあれば、プロパティを表示
+            self.layout.prop(context.object,'["file_name"]', text=self.bl_label)
+        else:
+            #プロパティがなければ、プロパティ追加ボタンを表示
+            self.layout.operator(MYADDON_OT_add_filename.bl_idname)
+
+
+        
+#パネル ファイル名
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname="myaddon.myaddon_ot_add_folename"
+    bl_label="FileName 追加"
+    bl_description="['file_name']カスタムプロパティを追加します"
+    bl_options={"REGISTER","UNDO"}
+
+    def execute(self,context):
+
+        #['faile_name']カスタムプロパティを追加
+        context.object["file_name"]=""
+
+        return {"FINISHED"}
+        
+        
 
 #blenderに登録するクラスリフト
 classes=(
@@ -132,6 +240,8 @@ MYADDON_OT_stretch_vertex,
 MYADDON_OT_create_ico_sphere,
 MYADDON_OT_export_scene,
 TOPBAR_MT_my_menu,
+MYADDON_OT_add_filename,
+OBJECT_PT_file_name,
 )
 
 
